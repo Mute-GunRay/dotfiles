7277309799795bd82281c3fa6425ae06d0bdd24e
Tate Ray <tate@tateray.com>
2025-05-15 15:18:45 -0600

    changes to org mode config;

--- .emacs.d/config.el
+++ .emacs.d/config.el
@@ -1,6 +1,45 @@
-;; bootstraping straight.el from its github repo
-;; builtin package.el is disabled via early-init file
+;; Strips down frame
+(scroll-bar-mode -1) ;; turns OFF scroll bar
+(menu-bar-mode -1) ;; turns OFF menu bar
+(tool-bar-mode -1) ;; turns OFF tool bar
+
+;; turn ON helpful modes
+(delete-selection-mode t)
+(global-auto-revert-mode t)
+(electric-pair-mode 1)
+
+;; turn OFF unneeded options
+(electric-indent-mode nil)
+(setq initial-scratch-message nil)
+
+
+;; mode specific hooks
+(add-hook 'prog-mode-hook 'display-line-numbers-mode)
+(add-hook 'text-mode-hook 'visual-line-mode)
+
+;; set backup directory
+(setq backup-directory-alist '((".*" . "~/.backups")))
+
+;; turn OFF auto save features
+(setq auto-save-default nil)
+(setq make-backup-files nil)
+(setq create-lockfiles nil)
+
+;; turns ON saving minibuffer history
+(savehist-mode 1)
+(setq history-length 25)
+
+;; turns ON recent file opening
+(recentf-mode 1)
+
+;; remember and restore last cursor location of opened file
+(save-place-mode 1)
+
+;; allow loading all themes without checking safety 
+(setq custom-safe-themes t)
+
 (defvar bootstrap-version)
+
 (let ((bootstrap-file
 	 (expand-file-name
 	  "straight/repos/straight.el/bootstrap.el"
@@ -9,23 +48,16 @@
 	(bootstrap-version 7))
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
-	  (url-retriev
-	   e-synchronously
+	  (url-retrieve-synchronously
 	   "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
 	   'silent 'inhibit-cookies)
 	(goto-char (point-max))
 	(eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 
-;;enables straight.el use-package integration
 (straight-use-package 'use-package)
 
 (defun set-exec-path-from-shell-PATH ()
-  "Set up Emacs' `exec-path' and PATH environment variable to match
-that used by the user's shell.
-
-This is particularly useful under Mac OS X and macOS, where GUI
-apps are not started from a shell."
   (interactive)
   (let ((path-from-shell (replace-regexp-in-string
 			    "[ \t\n]*$" "" (shell-command-to-string
@@ -40,39 +72,47 @@ apps are not started from a shell."
   :straight t
   :config (general-auto-unbind-keys))
 
+;; These are keybinds for basic functions
 (general-define-key
-  "C-q" 'backword-word
+ ;; cursor movement
+  "C-q" 'backward-word
   "C-e" 'forward-word
   "C-s" 'next-line
   "C-w" 'previous-line
   "C-a" 'backward-char
-  "C-d" 'forward-char)
+  "C-d" 'forward-char
+  ;; file manipulation
+  "C-f f" 'find-file
+  "C-f s" 'save-buffer
+  "C-f d" 'delete-file
+  "C-f c" 'copy-file
+  "C-f r" 'rename-file
+  "C-f n" 'recentf-open
+  ;; scroll buffer
+  "C-j" 'scroll-up-line
+  "C-k" 'scroll-down-line
+  "C-h" 'scroll-down-command
+  "C-l" 'sroll-up-command
+  ;; move between windows
+  "M-w" 'windmove-up
+  "M-s" 'windmove-down
+  "M-a" 'windmove-left
+  "M-d" 'windmove-right
+  ;; copy and past
+  "C-c u" 'undo
+  "C-c v" 'yank
+  "C-c x" 'kill-region
+  "C-c c" 'copy-region-as-kill
+  ;; split windows
+  "C-|" 'split-window-right
+  "C--" 'split-window-below)
 
+;; These are keybindings for org-mode
 (general-define-key
- "C-f f" 'find-file
- "C-f s" 'save-buffer)
+ "C-o a" 'org-agenda
+ "C-o t" 'org-create-table)
 
-(general-define-key
- "C-k" 'scroll-up-line
- "C-l" 'scroll-down-line)
-
-;; toggle builtin minor modes
-(scroll-bar-mode -1) ;; turns off vertical scroll bar
-(menu-bar-mode -1) ;; turns off top menu bar
-(tool-bar-mode -1) ;; turns off top tool bar
-(delete-selection-mode t) ;; allows delete and replace in region
-(electric-indent-mode nil) ;; turns off default re-indentation functionality
-(global-auto-revert-mode t) ;; auto updates buffers if file on disk has been changed
-(electric-pair-mode -1)
-;; add hooks for major modes
-(add-hook 'prog-mode-hook 'display-line-numbers-mode) ;; turns on line numbers for prog-mode
-(add-hook 'prog-mode-hook 'electric-pair-mode) ;; turns on electric pair mode for prog-mode
-(add-hook 'text-mode-hook 'visual-line-mode) ;; turn on word-wrapping when in text-mode
-
-;; set alists for some functions
-(setq backup-directory-alist '((".*" . "~/.backups")))
-
-(setq auto-save-default nil)
+;; These are keybindings for magit
 
 (use-package rainbow-delimiters
   :straight t)
@@ -90,18 +130,9 @@ apps are not started from a shell."
   :config
   (spacious-padding-mode 1))
 
-;; defaults
-(custom-set-faces
- '(default ((t :family "NovaMono" :height 160 :forground "#2A2B2A"))))
-(custom-set-faces
- '(variable-pitch ((t :family "Bitter" :height 120 :forground "#2A2B2A"))))
-
 (use-package org-modern
   :straight t
   :hook (org-mode . org-modern-mode))
-
-(add-to-list 'custom-theme-load-path  "~/.emacs.d/themes/")
-(load-theme 'cyberpunk-2019 t)
 
 (use-package corfu
   :straight t
@@ -128,10 +159,6 @@ apps are not started from a shell."
   :init
   (vertico-mode))
 
-(use-package savehist
-  :straight t
-  :init (savehist-mode))
-
 (use-package marginalia
   :straight t
   :init (marginalia-mode))
@@ -145,40 +172,32 @@ apps are not started from a shell."
 
 (setq auth-sources '("~/.authinfo"))
 
-(use-package denote
-  :straight t
-  :general
-  ("C-n" 'denote))
-
-(setq denote-directory (expand-file-name "~/Documents"))
-(setq denote-prompts '(subdirectory title keywords))
-
 (setq org-todo-keywords '("TODO(t)" "HOLDING(h)" "RESCHEDULE(r)" "|" "CANCELLED(c)" "DONE(d)"))
 
 (setq org-capture-templates
-	'(("t" "Task" entry
-	  (file buffer-name)
-	  "* %^{ TASK } [#%^{ PRIORITY | A | B | C }] %^g")
-	  ("s" "Sub Task" checkitem
-	   (file buffer-name)
-	   "%^{ TASK }")
-	  ("i" "Idea")
-	  ("ib" "Blog Post" entry
-	   (file buffer-name)
-	   "* %^{ IDEA } %^g")
-	  ("ig" "Game" entry
-	   (file buffer-name)
-	   "* %^{ IDEA } %^g")
-	  ("is" "Script" entry
-	   (file buffer-name)
-	   "*%^{ IDEA } %^g")
-	  ("b" "Blog Posts")
-	  ("bt" "Title" entry
-	   (file buffer-name)
-	   "* %^{ TITLE } %^g")
-	  ("bs" "Section" entry
-	   (file buffer-name)
-	   "** %^{ TITLE } \n %^?")))
+      '(("t" "Task" entry
+	 (file buffer-name)
+	 "* %^{ TASK } [#%^{ PRIORITY | A | B | C }] %^g")
+	("s" "Sub Task" checkitem
+	 (file buffer-name)
+	 "%^{ TASK }")
+	("i" "Idea")
+	("ib" "Blog Post" entry
+	 (file buffer-name)
+	 "* %^{ IDEA } %^g")
+	("ig" "Game" entry
+	 (file buffer-name)
+	 "* %^{ IDEA } %^g")
+	("is" "Script" entry
+	 (file buffer-name)
+	 "*%^{ IDEA } %^g")
+	("b" "Blog Posts")
+	("bt" "Title" entry
+	 (file buffer-name)
+	 "* %^{ TITLE } %^g")
+	("bs" "Section" entry
+	 (file buffer-name)
+	 "** %^{ TITLE } \n %^?")))
 
 (require 'org-habit)
 (add-to-list 'org-modules 'org-habit)
@@ -192,12 +211,18 @@ apps are not started from a shell."
 (setq org-agenda-skip-scheduled-if-done t)
 (setq org-agenda-custom-commands
       '(( "t" "Today"
-	  ((agenda "" ((org-agenda-span 'day)))))))
+  	((agenda "" ((org-agenda-span 'day)))))))
 
-(use-package org-superstar
-  :straight t
-  :after org
-  :hook (org-mode . org-superstar-mode))
+(add-hook 'org-mode-hook 'org-indent-mode)
+
+(use-package org-modern-indent
+  :straight (org-modern-indent :type git
+			       :host github
+			       :repo "jdtsmith/org-modern-indent")
+  :config
+  (add-hook 'org-mode-hook #'org-modern-indent-mode 90))
+
+(setq org-hide-emphasis-markers t)
 
 (use-package visual-fill-column
   :straight t
@@ -211,4 +236,44 @@ apps are not started from a shell."
   :config
   (add-hook 'after-init-hook #'global-flycheck-mode))
 (use-package flycheck-grammarly
+  :straight t)
+
+(use-package autothemer
+  :straight t)
+
+;; Configure Tempel
+(use-package tempel
+  ;; Require trigger prefix before template name when completing.
+  ;; :custom
+  ;; (tempel-trigger-prefix "<")
+
+  :bind (("M-+" . tempel-complete) ;; Alternative tempel-expand
+         ("M-*" . tempel-insert))
+
+  :init
+
+  ;; Setup completion at point
+  (defun tempel-setup-capf ()
+    ;; Add the Tempel Capf to `completion-at-point-functions'.
+    ;; `tempel-expand' only triggers on exact matches. Alternatively use
+    ;; `tempel-complete' if you want to see all matches, but then you
+    ;; should also configure `tempel-trigger-prefix', such that Tempel
+    ;; does not trigger too often when you don't expect it. NOTE: We add
+    ;; `tempel-expand' *before* the main programming mode Capf, such
+    ;; that it will be tried first.
+    (setq-local completion-at-point-functions
+                (cons #'tempel-expand
+                      completion-at-point-functions)))
+
+  (add-hook 'conf-mode-hook 'tempel-setup-capf)
+  (add-hook 'prog-mode-hook 'tempel-setup-capf)
+  (add-hook 'text-mode-hook 'tempel-setup-capf)
+
+  ;; Optionally make the Tempel templates available to Abbrev,
+  ;; either locally or globally. `expand-abbrev' is bound to C-x '.
+  ;; (add-hook 'prog-mode-hook #'tempel-abbrev-mode)
+  ;; (global-tempel-abbrev-mode)
+)
+
+(use-package tempel-collection
   :straight t)
